2ddb4ee99f4f51c732eb002d10f17cf7ea7f4b90
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-08-24 01:46:16 +0530

    Update AMSupdate.sh

--- device/AMSupdate.sh
+++ device/AMSupdate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-HELLO="not my first bash script"
-echo $HELLO
+echo "running AMS update.."
+wget https://github.com/Nandhu9999/agriculture_monitoring_system/archive/refs/heads/main.zip -O device.zip
+unzip subfolder.zip
