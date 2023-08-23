1f0f28f9615667ce0ccf2f18c7136df5c90b413f
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-08-24 02:12:49 +0530

    Update AMSupdate.sh

--- device/AMSupdate.sh
+++ device/AMSupdate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+File=config.json
+if test -f "$File"; then  
+  curl -H "Accept: application/json" -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Nandhu9999/agriculture_monitoring_system/main/device/config.json
+fi
 echo "running AMS update.."
 wget https://github.com/Nandhu9999/agriculture_monitoring_system/archive/refs/heads/main.zip -O device.zip
 unzip device.zip
@@ -10,3 +14,4 @@ mv agriculture_monitoring_system-main/device/* ./
 rm -r agriculture_monitoring_system-main/
 rm device.zip
 
+pip install -r requirements.txt
