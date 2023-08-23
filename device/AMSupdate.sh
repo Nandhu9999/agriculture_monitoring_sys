71d79b4dfd8957e474e874a238545ad41d28d93d
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-08-24 03:15:43 +0530

    Update AMSupdate.sh

--- device/AMSupdate.sh
+++ device/AMSupdate.sh
@@ -38,7 +38,6 @@ rm config.json main.py README.md requirements.txt
 
 mv agriculture_monitoring_system-main/device/* ./
 rm -r agriculture_monitoring_system-main/
-rm device.zip
 
 pip install -r requirements.txt
 python main.py
