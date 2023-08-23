2cf676f5e023a163a725cf508ecaa1b1d78602db
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-08-24 02:21:40 +0530

    Update AMSupdate.sh

--- device/AMSupdate.sh
+++ device/AMSupdate.sh
@@ -3,7 +3,9 @@
 File=config.json
 if test -f "$File"; then
   sudo apt-get install jq
-  key_value=$( curl -H "Accept: application/json" -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Nandhu9999/agriculture_monitoring_system/main/device/config.json | jq -r '.code_version')
+  my_code_version=$(jq -r ".$code_version" "$FILE")
+  code_version_latest=$( curl -H "Accept: application/json" -H "Cache-Control: no-cache" https://raw.githubusercontent.com/Nandhu9999/agriculture_monitoring_system/main/device/config.json | jq -r '.code_version')
+  echo $my_code_version $code_version_latest
 fi
 echo "running AMS update.."
 wget https://github.com/Nandhu9999/agriculture_monitoring_system/archive/refs/heads/main.zip -O device.zip
