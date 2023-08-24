65766a2e20e19bc70c5ffd78197c063d1f569a09
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-08-24 10:09:43 +0530

    Update AMSupdate.sh

--- device/AMSupdate.sh
+++ device/AMSupdate.sh
@@ -40,10 +40,11 @@ sudo mv agriculture_monitoring_system-main/device/* ./
 rm -r agriculture_monitoring_system-main/
 rm agriculture_monitoring_system-main.zip
 
-jsonStr = cat config.json
-serial_no = cat /sys/firmware/devicetree/base/serial-number
+jsonStr = $(cat config.json)
+serial_no = $(cat /sys/firmware/devicetree/base/serial-number)
 jq '.serial_no = "$serial_no"' <<<"$jsonStr"
 echo $jsonStr > config.json
+echo "SERIAL NO =" $serial_no
 
 pip install -r requirements.txt
 python main.py
