dff3cd9c9a21702066e8e49657cc204979c52a7d
Nandhu <87135537+Nandhu9999@users.noreply.github.com>
2023-08-30 01:45:10 +0530

    Update AMSupdate.sh

--- device/AMSupdate.sh
+++ device/AMSupdate.sh
@@ -17,13 +17,16 @@ dfolder="/ams-main/device/"
 pip install -r "$HOME$dfolder"requirements.txt
 
 update_json_serial() {
-  local jsonStr=$(cat "$HOME$dfolder"config.json)
   echo "***********************************"
-  echo $jsonStr
-  local serial_no=$(cat /sys/firmware/devicetree/base/serial-number)
-  local updated_json=$(jq --arg serial "$serial_no" '.serial_no = $serial' <<<"$jsonStr")
-  echo "$updated_json" > "$HOME$dfolder""config.json"
-  echo "SERIAL NO = $serial_no"
+  # Read the JSON data using jq
+  jsonStr=$(cat "$HOME$dfolder"config.json)
+  # Read the serial number from the device
+  serial_no=$(cat /sys/firmware/devicetree/base/serial-number)
+  # Update the JSON using jq
+  updated_json=$(echo "$jsonStr" | jq --arg serial "$serial_no" '.serial_no = $serial')
+  # Save the updated JSON back to the file
+  echo "$updated_json" > "$HOME$dfolder"config.json
+  echo "Updated config.json with serial_no: $serial_no"
   echo "***********************************"
 }
 update_json_serial
